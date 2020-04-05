af5a85302a902b94a7cdf60bc9211ebf1d64d373
pjh37 <37110261+pjh37@users.noreply.github.com>
2020-04-06 00:27:17 +0900

    1. 그룹 만들기 ui추가 2. 그룹 fragment ui변경

--- app/src/main/java/com/example/graduationproject/community/fragment/GroupFragment.java
+++ app/src/main/java/com/example/graduationproject/community/fragment/GroupFragment.java
@@ -1,26 +1,53 @@
 package com.example.graduationproject.community.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.graduationproject.R;
+import com.example.graduationproject.community.activity.GroupCreateActivity;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class GroupFragment extends Fragment {
+    Button btnGroupCreate;
+    Button btnCategory;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup rootView=(ViewGroup)inflater.inflate(R.layout.community_fragment_group,container,false);
+        btnGroupCreate=(Button)rootView.findViewById(R.id.btnGroupCreate);
+        btnCategory=(Button)rootView.findViewById(R.id.btnCategory);
+        btnGroupCreate.setOnClickListener(new ClickListener());
+        btnCategory.setOnClickListener(new ClickListener());
+
         return rootView;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+    }
+    public class ClickListener implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.btnGroupCreate:{
+                    Intent intent=new Intent(getContext(), GroupCreateActivity.class);
+                    startActivity(intent);
+                    break;
+                }
+                case R.id.btnCategory:{
+
+                    break;
+                }
+            }
+        }
     }
 }
